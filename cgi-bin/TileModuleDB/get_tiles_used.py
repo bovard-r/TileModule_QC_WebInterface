be7f1703d24e0739325bc7b44cdb0ed4abaa501e
Jeremiah Mans <jmmans@umn.edu>
2026-02-09 18:15:25 +0100

    Working through main steps, needs start point and a little clearer endpoint

---
+++ cgi-bin/TileModuleDB/get_tiles_used.py
@@ -0,0 +1,362 @@
+#!/usr/bin/python
+import sys
+import connect
+from collections import Counter
+
+# Yes CGI is deprecated, but that's how things are
+import cgi
+import html
+
+tile_config={
+    'D8F' : {
+        25: (24, 24, 24, 24, 24, 24, 'S4', 'S4'),
+        24: (24, 24, 24, 24, 24, 24, 'S4', 'S4'),
+        23: (22, 'S4', 22, 22, 'S4', 22, 'S4', 22),
+        22: (22, 22, 22, 22, 22, 22, 22, 22),
+        21: (20, 20, 20, 20, 20, 20, 20, 20),
+        20: (20, 20, 20, 20, 20, 20, 20, 20),
+        19: (18, 18, 18, 18, 18, 18, 18, 18),
+        18: (18, 'S3', 18, 18, 18, 18, 'S3', 18),
+    },
+    'B2F' : {
+        17: (16, 16, 'S3', 'S3', 16, 16, 16, 16),
+        16: (16, 16, 'S3', 'S3', 16, 16, 'S3', 16),
+        15: (14, 'S3', 14, 14, 'S3', 14, 14, 14),
+        14: (14, 14, 14, 14, 14, 14, 14, 14),
+        13: (12, 12, 12, 12, 12, 12, 12, 12),
+        12: (12, 12, 12, 12, 12, 12, 12, 12),
+        11: (10, 10, 10, 10, 10, 10, 10, 10),
+        10: (10, 10, 10, 10, 10, 10, 10, 10),
+        9 : (8, 8, 8, 8, 8, 8, 8, 8),
+        8 : (8, 8, 8, 8, 8, 8, 8, 8),
+        7 : (6, 'S2', 6, 6, 6, 6, 'S2', 6),
+        6 : (6, 6, 6, 6, 6, 6, 6, 6)
+    },
+    'G3F' : {
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 'S5', 34),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G3L' : {
+        36 : (None, 36, 36, 36, 36, 36, 36, 36),
+        35 : (None, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : (None, 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G3R' : {
+        36 : (36, 36, 36, 36, 36, 36, 36, None),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 'S5',None),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', None)
+    },
+    'G5F' : {
+        38 : (38, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 'S6', 36, 36, 36, 36, 'S6', 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G5L' : {
+        38 : (None, 38, 38, 38, 38, 38, 38, 38),
+        37 : (None, 'S6', 36, 36, 36, 36, 'S6', 36),
+        36 : (None, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G5R' : {
+        38 : (38, 38, 38, 38, 38, 38, 38, None),
+        37 : (36, 'S6', 36, 36, 36, 36, 'S6', None),
+        36 : (36, 36, 36, 36, 36, 36, 36, None),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G7F' : {
+        40 : (40, 40, 40, 40, 40, 40, 40, 40),
+        39 : (38, 'S6', 36, 38, 38, 38, 'S6', 38),
+        38 : (38, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G7L' : {
+        40 : (None, 40, 40, 40, 40, 40, 40, 40),
+        39 : (None, 'S6', 36, 38, 38, 38, 'S6', 38),
+        38 : (None, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G7R' : {
+        40 : (40, 40, 40, 40, 40, 40, 40, None),
+        39 : (38, 'S6', 36, 38, 38, 38, 'S6', None),
+        38 : (38, 38, 38, 38, 38, 38, 38, None),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G8F' : {
+        41 : (40, 40, 40, 40, 40, 40, 40, 40),
+        40 : (40, 40, 40, 40, 40, 40, 40, 40),
+        39 : (38, 'S6', 36, 38, 38, 38, 'S6', 38),
+        38 : (38, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G8L' : {
+        41 : (None, 40, 40, 40, 40, 40, 40, 40),
+        40 : (None, 40, 40, 40, 40, 40, 40, 40),
+        39 : (38, 'S6', 36, 38, 38, 38, 'S6', 38),
+        38 : (38, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    },
+    'G8R' : {
+        41 : (40, 40, 40, 40, 40, 40, 40, None),
+        40 : (40, 40, 40, 40, 40, 40, 40, None),
+        39 : (38, 'S6', 36, 38, 38, 38, 'S6', 38),
+        38 : (38, 38, 38, 38, 38, 38, 38, 38),
+        37 : (36, 36, 36, 36, 36, 36, 36, 36),
+        36 : (36, 36, 36, 36, 36, 36, 36, 36),
+        35 : (34, 'S5', 34, 34, 'S5', 34, 34, 'S5'),
+        34 : ('S5', 'S5', 'S5', 'S5', 'S5', 'S5', 'S5', 34)
+    }
+}
+
+def header(of, which):
+    of.write('''
+    <html><head>
+    <style>
+    td, select {
+    text-align: center; font-size: x-large; font-family: sans-serif;
+    background-color: #FFFFFF;
+    }
+    .missing {
+    background-color: #111111;
+    }
+    .submitter {
+    font-size: x-large; font-family: sans-serif;
+    }
+    th, .ring {
+    text-align: center; font-size: xx-large; font-family: sans-serif;
+    background-color: #1111ff; color: yellow; 
+    width: 10%;
+    }
+    .stile {
+    background-color: yellow;
+    }
+    </style>
+    </head><body>
+    ''')
+    
+
+def format_id(id):
+    return id[0:3]+"-"+id[3:5]+"-"+id[5:9]+"-"+id[9:]
+
+def gather_tile_info(cur):
+    query="SELECT COUNT(*), batch, typecode from COMPONENT_STOCK WHERE alt_barcode is NULL AND component_id NOT IN (SELECT component_id from COMPONENT_USAGE) GROUP BY typecode"
+    cur.execute(query)
+    tileinfo={}
+    for row in cur:
+        if row[2] not in tileinfo:
+            tileinfo[row[2]]={}
+        tileinfo[row[2]][row[1]]=int(row[0])
+    return tileinfo
+
+def find_qc_tile(cur, barcode):
+    query="SELECT alt_barcode FROM COMPONENT_STOCK where barcode='%s'  AND component_id NOT IN (SELECT component_id from COMPONENT_USAGE)"%barcode
+    cur.execute(query)
+    for row in cur:
+        return row[0]
+    return None
+
+def pick_tiles(cur, tiletype, batches):
+    tiles={}
+    for batch in batches:
+        query="SELECT barcode FROM COMPONENT_STOCK WHERE typecode='%s' AND batch='%s' AND alt_barcode is NULL AND component_id NOT IN (SELECT component_id from COMPONENT_USAGE) ORDER BY barcode LIMIT %d"%(tiletype,batch[2:],batches[batch])
+        cur.execute(query)
+        tiles[batch]=[]
+        for row in cur:
+            tiles[batch].append(row[0])
+    return tiles
+
+def make_tile_array(cur,of,tmbc):
+    tm=tmbc[5:8]
+
+    tileinfo=gather_tile_info(cur)
+
+    of.write('<h1>Tile Assignment for %s</h1>'%format_id(tmbc))
+    
+    of.write('<form id="tileform" method="GET" action="get_tiles_used.py">\n')
+    of.write('<input type=hidden name="barcode" value="%s">\n'%tmbc)
+    of.write('<input type=hidden name="step" value="tile_assignment_verify">\n')
+    of.write("<table border=2 width=100%%>\n")
+
+    of.write("<tr><th><th>7<th>6<th>5<th>4<th>3<th>2<th>1<th>0</tr>\n")
+
+    selects=[]
+    bc=[]
+    for ring in sorted(tile_config[tm].keys(), reverse=True):        
+        of.write("<tr><td class=ring><b>%d</b>"%ring)
+        iphi=7
+        for tile in tile_config[tm][ring]:
+            if tile is None:
+                of.write("<td class=missing>")
+                continue
+            elif isinstance(tile,str):
+                of.write("<td class=stile>%s<br>"%tile)
+            else:
+                of.write("<td>%s<br>"%tile)
+            name="tile_%02d_%02d"%(iphi,ring)
+            of.write('<select name=%s><option value="QC">QC'%name)
+            info=tileinfo['TI-%s'%tile]
+            maxbatchn=max(info, key=info.get)
+            for sb in info:
+                of.write('<option value="SB%s" %s>SB%s'%(sb,"selected" if sb==maxbatchn else "",sb))
+            of.write('</select>\n')
+            selects.append(name)
+            name="bc_%02d_%02d"%(iphi,ring)            
+            of.write("<br><input type='text' size=15 name=%s id=%s>\n"%(name,name))
+            bc.append(name)
+            iphi=iphi-1
+            
+        of.write("</tr>\n")
+
+    of.write("</table>\n")
+    of.write("<p>\n<center><input class=submitter type=submit value='Submit Tile Assignment'></center>\n")
+    of.write("</form>\n")
+    of.write("<script>\n")
+    of.write('''
+    function show(el, state) {
+	if (state === undefined) {
+		state = true;
+	}
+    if (state === true) {
+		el.style.display = '';
+    } else {
+        el.style.display = 'none';
+    }
+}
+function enableIfSelected() {
+    var select = this,
+    optionValue = select.options[select.selectedIndex].value,
+    el;
+    const bcname="bc"+this.name.slice(4,5+5);
+    el=document.getElementById(bcname);
+    show(el, ("QC" === optionValue));
+ }
+function attachEnableIfSelected(select) {
+	select.onchange = enableIfSelected;
+	select.onchange();
+}''')
+    of.write("var form=document.getElementById('tileform');\n")
+    for i in range(0,len(selects)):
+        of.write("attachEnableIfSelected(form.elements.%s);\n"%(selects[i]))
+    of.write("</script>\n")
+
+def verify_selection(cur, of, info):
+    byring={}
+    bytile={}
+    assigned={}
+    tm=info.getvalue("barcode")[5:8]
+    pref="TI-" # needs work!!!!
+    mymemory={}
+    for name in info.keys():
+        value=info.getvalue(name)
+        if name[:4]!='tile': continue
+        iphi=int(name[5:7])
+        ring=int(name[8:10])
+        if ring not in byring:
+            byring[ring]=[None]*8
+        byring[ring][iphi]=value
+        tiletype=tile_config[tm][ring][iphi]
+        if tiletype not in bytile:
+            bytile[tiletype]=Counter()
+        if value=='QC':
+            sb_tile=find_qc_tile(cur,info.getvalue("bc_%02d_%02d"%(iphi,ring)))
+            if sb_tile is None:
+                of.write("<h1>Unable to find tile with barcode '%s' for %s,%s</h1>"%(info.getvalue("bc_%02d_%02d"%(iphi,ring)),ring,iphi))
+                return
+            mymemory['tile_%02d_%02d'%(iphi,ring)]=sb_tile
+        else:
+            bytile[tiletype][value]+=1
+
+    for tiletype in bytile:
+        assigned[tiletype]=pick_tiles(cur, pref+str(tiletype), bytile[tiletype])
+    
+    of.write("<table><tr><th>Ring<th>IPhi<th>Batch<th>Barcode</tr>")
+    for ring in sorted(byring.keys()):
+        for iphi in range(0,8):
+            if byring[ring][iphi] is not None:
+                barcode=""
+                tiletype=tile_config[tm][ring][iphi]
+                if tiletype is None:
+                    continue
+                byr=byring[ring][iphi]
+                if byr=='QC':
+                    barcode=mymemory['tile_%02d_%02d'%(iphi,ring)]
+                else:
+                    if len(assigned[tiletype][byr])==0:
+                        print("Ran out of %s!"%tiletype)
+                        continue
+                    barcode=assigned[tiletype][byr][0]
+                    mymemory['tile_%02d_%02d'%(iphi,ring)]=barcode
+                    del assigned[tiletype][byr][0]
+                                                                  
+                of.write("<tr><td>%s<td>%d<td>%s<td>%s</tr>\n"%(ring,iphi,byring[ring][iphi],barcode))
+    of.write("</table>")
+    of.write('<form id="tileform" method="GET" action="get_tiles_used.py">\n')
+    of.write('<input type=hidden name="barcode" value="%s">\n'%info.getvalue("barcode"))
+    of.write('<input type=hidden name="step" value="tile_assignment_commit">\n')
+    for (item,value) in mymemory.items():
+        of.write('<input type=hidden name="%s" value="%s">\n'%(item,value))
+    of.write("<p>\n<center><input class=submitter type=submit value='Commit Tile Assignment'></center></form>\n")
+
+def commit_selection(cur, of, info):
+    making=info.getvalue("barcode")
+    for item in info.keys():
+        if item[:4]!='tile':
+            continue
+        iphi=int(item[5:7])
+        ring=int(item[8:10])
+        # fancy insert didn't work
+        #        query='INSERT INTO COMPONENT_USAGE (component_id, used_in_barcode, used_iphi, used_ring) VALUES ( (SELECT component_id FROM COMPONENT_STOCK WHERE barcode="%s"),"%s",%d,%d)'%(info.getvalue(item),making,iphi,ring)
+        query='SELECT component_id FROM COMPONENT_STOCK WHERE barcode="%s" OR alt_barcode="%s"'%(info.getvalue(item),info.getvalue(item),)
+        cur.execute(query)
+        id=None
+        for row in cur:
+            id=int(row[0])
+        if id is None:
+            of.write("Problem with %d %d -> %s"%(iphi,ring,info.getvalue(item)))
+            continue
+                     
+        query='INSERT INTO COMPONENT_USAGE (component_id, used_in_barcode, used_iphi, used_ring, used_when) VALUES (%d,"%s",%d,%d,NOW())'%(id,making,iphi,ring)            
+        cur.execute(query)
+    
+print("Content-type: text/html\n")
+
+db=connect.connect(0)
+cur=db.cursor()
+form=cgi.FieldStorage()
+
+step=form.getvalue('step','start')
+
+if step=='tile_assignment':
+    header(sys.stdout, form['step'])
+    make_tile_array(cur, sys.stdout,"320TMG5RCF00011")
+elif step=='tile_assignment_verify':
+    header(sys.stdout, form['step'])
+    verify_selection(cur, sys.stdout,form)
+elif step=='tile_assignment_commit':
+    db=connect.connect(1)
+    cur=db.cursor()
+    header(sys.stdout, form['step'])
+    commit_selection(cur, sys.stdout,form)
+    db.commit()
+else:
+    header(sys.stdout, None)    
+    pick_tileboard(sys.stdout)
