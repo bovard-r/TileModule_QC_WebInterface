42d49b24eaf7e2d54155789adda4f3a3bd88fc76
lab6-tilecal-01 <lab6-tilecal-01@fnal.gov>
2026-03-09 17:02:24 -0500

    Also update component usage to follow the board

--- cgi-bin/TileModuleDB/board_checkin.py
+++ cgi-bin/TileModuleDB/board_checkin.py
@@ -41,9 +41,9 @@ if action == "submit":
         sn = module[9:15]
 
         type_id = module[3:9]
-
         cur.execute('update Board set sn="%s", full_id="%s", type_id="%s" where full_id="%s"' % (sn, module, type_id, tq))
         db.commit()
+        cur.execute('update COMPONENT_USAGE set used_in_barcode="%s" where used_in=(SELECT board_id FROM Board where full_id="%s")' % module)
 
         cur.execute("select * from Board where full_id='%s'" % module)
         rows = cur.fetchall()
